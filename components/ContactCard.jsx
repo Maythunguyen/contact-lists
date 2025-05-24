39877c313753016cd763526df29db50557d3d6ba
May Thu Nguyen <thumay@Thus-MacBook-Air.local>
2025-05-25 07:52:19 +1000

    update cards

--- components/ContactCard.jsx
+++ components/ContactCard.jsx
@@ -10,11 +10,12 @@ import {
 } from "@tabler/icons-react";
 
 
+
 const ContactCard = ({ contact }) => {
   return (
     <div className="w-full max-w-sm rounded-xl overflow-hidden bg-white shadow-subtle dark:bg-neutral-800 transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-lg">
       {/* HEADER SECTION */}
-      <div className="flex items-center space-x-4 px-4 py-5 rounded-t-xl border-b border-gray-100">
+      <div className="flex items-center space-x-5 px-6 py-6 rounded-t-xl border-b border-gray-100">
         <img
           src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Avatar"
@@ -27,27 +28,27 @@ const ContactCard = ({ contact }) => {
       </div>
 
       {/* BODY SECTION */}
-      <div className="px-5 py-4 space-y-3 text-sm text-gray-600 dark:text-gray-300">
-        <div className="flex items-center gap-3">
-          <IconMail className="h-5 w-5 text-gray-500" />
+      <div className="px-6 py-6 space-y-4 text-sm text-gray-600 dark:text-gray-300">
+        <div className="flex items-center gap-5">
+          <IconMail className="h-4 w-4 text-gray-500" />
           <span className="font-medium text-gray-800 dark:text-white">
             {contact.email}
           </span>
         </div>
-        <div className="flex items-center gap-3">
-          <IconPhone className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center gap-5">
+          <IconPhone className="h-4 w-4 text-gray-500" />
           <span>{contact.phone}</span>
         </div>
-        <div className="flex items-center gap-3">
-          <IconBrandDribbble className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center gap-5">
+          <IconBrandDribbble className="h-4 w-4 text-gray-500" />
           <span>{contact.website}</span>
         </div>
-        <div className="flex items-center gap-3">
-          <IconMapPin className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center gap-5">
+          <IconMapPin className="h-4 w-4 text-gray-500" />
           <span>{contact.address.city}</span>
         </div>
-        <div className="flex items-center gap-3">
-          <IconBriefcase className="h-5 w-5 text-gray-500" />
+        <div className="flex items-center gap-5">
+          <IconBriefcase className="h-4 w-4 text-gray-500" />
           <span>{contact.company.name}</span>
         </div>
       </div>
